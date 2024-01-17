cc33d11a4aecac6fb5bb2ca68cb29f213109ba26
Oscar Cely <oscar@gmail.com>
2024-01-17 17:56:46 -0500

    update init file

--- config/__init__.py
+++ config/__init__.py
@@ -5,7 +5,7 @@ from api.framework.routers import root
 
 app = FastAPI(
     title="Fastapi base template for Python API projects",
-    description="Base template project to expand your ideas without need to build the project from scratch.",
+    description="Base template project.",
     version="v1",
     docs_url="/docs"
 )
